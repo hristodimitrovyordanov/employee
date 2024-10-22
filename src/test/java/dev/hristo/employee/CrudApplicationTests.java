7d2dcb4f82b7afd64f7d0142a2de51b73a0cfa48
Hristo Dimitrov Yordanov <mrhristo29@gmail.com>
2024-10-22 18:21:05 +0300

    Starting new crud project.

---
+++ src/test/java/dev/hristo/employee/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hristo.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
